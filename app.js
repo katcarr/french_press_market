893dbd672bc68292cc79966937a6803928d943a8
Kathryn Carr and Ron Park <ronpark9@gmail.com>
2015-02-24 16:37:23 -0800

    Update JS files to include functions to calculate totals

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-var frenchPressMarket = angular.module("frenchPressMarket", []);
+var frenchPressMarket = angular.module("frenchPressMarket", ['ui.bootstrap']);
+
+frenchPressMarket.controller('ButtonsCtrl', function ($scope) {
+  $scope.singleModel = 1;
+
+  $scope.radioModel = 'Middle';
+
+  $scope.checkModel = {
+    left: false,
+    middle: true,
+    right: false
+  };
+});
--- services/FrenchPressFactory.js
+++ services/FrenchPressFactory.js
@@ -1,11 +1,37 @@
 frenchPressMarket.factory('FrenchPressFactory', function FrenchPressFactory() {
   var factory = {};
   factory.presses = [];
+  factory.total = 0;
 
   factory.addPress = function() {
-    var press = {};
-
+    var press = {volume: factory.pressSize, material: factory.pressMaterial, quantity: factory.pressQuantity};
     factory.presses.push(press);
+
+    if (press.volume === "Pint") {
+      factory.total = 10 * press.quantity; // Pint Metal
+      if (press.material === "Glass") {
+        factory.total = 5 * press.quantity; // Pint Glass
+      }
+    } else if (press.volume === "Quart") {
+      factory.total = 20 * press.quantity; // Quart Metal
+      if (press.material === "Glass") {
+        factory.total = 10 * press.quantity; // Quart Glass
+      }
+    } else {
+      factory.total = 30 * press.quantity; // Pitcher Metal
+      if (press.material === "Glass") {
+        factory.total = 20 * press.quantity; // Pitcher Glass
+      }
+    }
+
+    if (press.quantity > 100) {
+      factory.total *= 0.80;
+    } else if (press.quanity < 101 && press.quantity > 49) {
+      factory.total *= 0.90;
+    } else if (press.quantity > 24) {
+      factory.total *= 0.95;
+    }
+
   };
 
   factory.deletePress = function(press) {
